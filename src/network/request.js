4094b37638f279fd2ec8e17836105d6538afb874
yyb960412 <59006710+yyb960412@users.noreply.github.com>
2020-04-07 00:08:01 +0800

    Update request.js
    新接口无法提供

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config){
   //1.创建vue实例
   const instance = axios.create({
     // baseURL: 'http://123.207.32.32:8000',
-    baseURL: 'http://106.54.54.237:8000',
+    baseURL: 'http://106.54.54.237:8000/api/xxx',
     timeout: 5000
   })
 
