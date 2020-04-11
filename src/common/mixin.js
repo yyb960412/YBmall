c2685e440abaa5e0ea5f390a8a6768f3f8b4e309
yyb960412 <59006710+yyb960412@users.noreply.github.com>
2020-04-11 11:08:04 +0800

    Update mixin.js
    add mixin function

--- src/common/mixin.js
+++ src/common/mixin.js
@@ -32,3 +32,27 @@ export const backTopMixin = {
     }
   }
 }
+
+export const tabControlMixin = {
+  data: function(){
+    return {
+      currentType: 'pop'
+    }
+  },
+  methods:{
+    tabClick(index){
+      switch(index){
+        case 0:
+          this.currentType = 'pop'
+          break
+        case 1:
+          this.currentType = 'new'
+          break
+        case 2:
+          this.currentType = 'sell'
+          break
+      }
+      console.log(this.currentType)
+    }
+  }
+}
