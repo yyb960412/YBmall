8915db794474d004a8a4e5897100ba5e63c18b62
yyb960412 <59006710+yyb960412@users.noreply.github.com>
2020-04-11 10:13:35 +0800

    Add files via upload
    完成分类页面制作

---
+++ src/views/category/childComp/TabContentDetail.vue
@@ -0,0 +1,98 @@
+<template>
+  <grid-view>
+     <!-- <goods-list-item v-for="(item, index) in categoryDetail" :key="index" :goodsItem="item"></goods-list-item> -->
+    <div v-for="(item,index) in categoryDetail" :key="index">
+       <div class="goods-item" @click="gItemClick"> -->
+        <!-- <img v-lazy="showImage" :key="showImage" @load="imageLoad"> -->
+        <img :src="item.img" @load="imageLoad">
+        <div class="goods-info">
+          <p>{{item.title}}</p>
+          <span class="price">{{item.price}}</span>
+          <span class="collect">{{item.cfav}}</span>
+        </div>
+      </div>
+    </div>
+  </grid-view>
+</template>
+
+<script>
+import GridView from 'components/common/gridview/GridView.vue'
+import GoodsListItem from 'components/content/goods/GoodsListItem.vue'
+
+export default {
+  name: 'TabContentDetail',
+  components:{
+    GridView,
+    GoodsListItem,
+  },
+  props:{
+    categoryDetail:{
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  },
+  methods:{
+    imageLoad(){
+      //  if(this.$route.path.indexOf('/home')){
+      //   this.$bus.$emit("itemImageLoad")
+      // }
+    },
+    gItemClick(){
+      this.$router.push('/detail/' + this.item.iid)
+    }
+  }
+}
+</script>
+
+<style>
+  .goods-item {
+    padding-bottom: 50px;
+    position: relative;
+
+    width: 50%;
+  }
+
+  .goods-item img {
+    width: 100%;
+    border-radius: 5px;
+    /* border:3px solid var(--color-tint); */
+  }
+
+  .goods-info {
+    font-size: 12px;
+    position: absolute;
+    bottom: 5px;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+    text-align: center;
+  }
+
+  .goods-info p {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin-bottom: 3px;
+  }
+
+  .goods-info .price {
+    color: var(--color-high-text);
+    margin-right: 20px;
+  }
+
+  .goods-info .collect {
+    position: relative;
+  }
+
+  .goods-info .collect::before {
+    content: '';
+    position: absolute;
+    left: -15px;
+    top: -1px;
+    width: 14px;
+    height: 14px;
+    background: url("~assets/images/common/collect.svg") 0 0/14px 14px;
+  }
+</style>
---
+++ src/views/category/childComp/TabMenu.vue
@@ -0,0 +1,57 @@
+<template>
+    <scroll class="tab-menu">
+      <!-- <div class="menu-list"> -->
+        <div class="menu-list-item" :class="{active: index===currentIndex}" v-for="(item, index) in categories" :key="index" @click="itemClick(index)">
+          {{item.title}}
+        </div>
+      <!-- </div> -->
+    </scroll>
+</template>
+
+<script>
+import Scroll from 'components/common/scroll/Scroll.vue'
+export default {
+  name: 'TabMenu',
+  components: {
+    Scroll
+  },
+  props:{
+    categories: Array
+  },
+  data(){
+    return{
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index){
+      // console.log(index)
+      this.currentIndex = index
+      this.$emit('selectItem', index)
+    }
+  }
+}
+</script>
+
+<style>
+  .tab-menu{
+    background-color: #f6f6f6;
+    height: 100%;
+    width: 100px;
+    box-sizing: border-box;
+  }
+
+  .menu-list-item{
+    height: 45px;
+    line-height: 45px;
+    text-align: center;
+    font-size: 14px;
+  }
+
+  .menu-list-item .active{
+    font-weight: 700;
+    color: var(--color-high-text);
+    background-color: #f6f6f6;
+    border-left: 3px solid var(--color-high-text);
+  }
+</style>
